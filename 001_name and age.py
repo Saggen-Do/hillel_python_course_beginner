fa2add447b5b0256e6eb4f65865f3a22247ca998
Vladymyr Buchko <105335074+Saggen-Do@users.noreply.github.com>
2022-07-16 16:26:29 +0300

    Update 001_name and age.py

--- 001_name and age.py
+++ 001_name and age.py
@@ -4,8 +4,8 @@ old = list(range(70, 91, 1))
 too_old = list(range(91, 121, 1))
 
 user_name = input("Enter your name: ")
-user_age = int(input())
-
+user_age = int(input("Enter your age: ")
+               
 if user_age == 16:
     print(user_name, ", you need to wait one year")
 elif user_age in young:
